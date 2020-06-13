8dd83d97b994c42215316a42495ef4ab680f98c9
Daniel Gorianz <aalbaafikaa@gmail.com>
2020-06-13 17:02:30 -0400

    login

---
+++ www/js/index.js
@@ -0,0 +1,18 @@
+
+var app = {
+
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+
+    receivedEvent: function(id) {
+       
+    }
+};
+
+app.initialize();
---
+++ www/js/login.js
@@ -0,0 +1,29 @@
+
+// const V = [34,24,57,55,766,343,56,1,34,0];
+
+
+// function cambiar(num1,num2){
+
+//     let aux = V[num1] 
+//     V[num1] = V[num2] 
+//     V[num2] = aux 
+
+//     console.log(V)
+//     debugger
+// }
+// (function ordernar(){
+
+//     for (let i = 1; i < V.length; i++) {
+
+//         let p = i
+//         let pos = i-1 
+//         let aux = V[i]; 
+
+//         while (V[pos] >= 0 && V[pos] > aux) {
+//             cambiar(p, pos)
+//             p--;        
+//             pos--;
+        
+//         }
+//     }
+// })();
